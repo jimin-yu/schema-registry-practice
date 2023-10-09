872177c30a61dcca4fe31580c2dce57f5f1dacff
유지민 <86466319+jimin-yu@users.noreply.github.com>
2023-10-09 21:33:50 +0900

    feat: ruby consumer

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
+Kafka Schema Registry
+=====================
+
+schema registry 없이 사용
+![](images/withSchemaRegistry.webp)
+
+schema registry => 메세지 스키마가 올바른지 검증 가능
+![](images/withSchemaRegistry.webp)
+
+여러가지 구현체가 있으나 그 중 confluent 사에서 만든 confluent schema registry가 가장 많이 쓰임.
+여기서 채택한 데이터 포맷이 AVRO
+
+### INFO
+```
+kafka
+localhost:9094
+
+schema-registry
+localhost:8081
+
 ui
 localhost:9090
+```
+
+### run locally
+```bash
+docker-compose up -d
+```
+
+
 
--- ruby-app/producer.rb
+++ ruby-app/producer.rb
@@ -1,17 +1,53 @@
+require 'avro_turf'
+require 'avro_turf/messaging'
 require 'kafka'
 
-# Kafka 클러스터에 연결
-kafka = Kafka.new(
-  seed_brokers: ['localhost:9094'],
-  client_id: 'my-producer',
-)
+SCHEMA_REGISTRY_URL = 'http://localhost:8081'
+KAFKA_BROKER_URL = 'localhost:9094'
 
-topic = 'sample-topic'
-producer = kafka.producer
+########################## FUNCTIONS ##############################
 
-# 메시지 보내기
-producer.produce('Hello, Kafka!', topic: topic)
+# Avro Schema + Encoded Data
+def encoding_in_data_file_format(data)
+  # [schema_path] *.avsc 파일 저장되어 있는 디렉토리 위치
+  avro = AvroTurf.new(schemas_path: 'schemas/')
+  encoded_data = avro.encode(data, schema_name: "person")
+  # "Obj\u0001\u0004\u0014avro.codec\bnull\u0016avro.schema\xFA\u0002{\"type\":\"record\",\"name\":\"person\",\"fields\":[{\"name\":\"first_name\",\"type\":\"string\",\"doc\":\"이름\"},{\"name\":\"last_name\",\"type\":\"string\",\"doc\":\"성\"},{\"name\":\"age\",\"type\":\"int\",\"doc\":\"나이\"}]}\u0000\\\xFC\xFE\xF9G\xB5\xF2\u0006\\ЃY\f\xB8\v;\u0002\u0014\nJimin\u0004Yu6\\\xFC\xFE\xF9G\xB5\xF2\u0006\\ЃY\f\xB8\v;"
+  encoded_data
+end
 
-# 프로듀서를 닫습니다.
-producer.deliver_messages
-producer.shutdown
+# kafka, database 저장용
+# 앞에 schema가 붙어있는 data file format 대신 사이즈 경량화 할 때 
+# (사용 예시 스키마 레지스트리를 이용하는 카프카 메세징)
+# Messaging API를 사용해야함 (include avro_turf/messaging)
+# NOTE: The Messaging format is **not compatible** with the Avro data file API.
+def encoding_in_messaging_format(data)
+  avro = AvroTurf::Messaging.new(registry_url: SCHEMA_REGISTRY_URL)
+  # Schema registry에 없으면 등록함.
+  # I, [2023-10-09T11:48:15.894704 #18123]  INFO -- : Registered schema for subject `person`; id = 1
+  encoded_message = avro.encode(data, schema_name: "person")
+  # "\u0000\u0000\u0000\u0000\u0001\nJimin\u0004Yu6"
+  encoded_message
+end
+
+def produce_message_to_topic(message, topic)
+  kafka = Kafka.new(
+    seed_brokers: [KAFKA_BROKER_URL],
+    client_id: 'my-producer',
+  )
+  producer = kafka.producer
+  producer.produce(message, topic: topic)
+  producer.deliver_messages
+  producer.shutdown
+end
+
+####################################################################
+
+user_data = {
+  "first_name" => 'Jimin',
+  "last_name" => "Yu",
+  "age" => 24
+}
+encoded_message = encoding_in_messaging_format(user_data)
+produce_message_to_topic(encoded_message, 'sample-topic')
+p 'DONE'
